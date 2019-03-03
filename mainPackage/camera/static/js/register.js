50631c664a99f6e0a38e7d9c1f9069ac4e49ed0e
Shesky17 <30205154+Shesky17@users.noreply.github.com>
2019-03-03 17:25:06 -0500

    URGENT. Fixed something. Run server.py to run the game. Changed whole project structure.

---
+++ mainPackage/camera/static/js/register.js
@@ -0,0 +1,36 @@
+var initHP = 2;
+var initLoc = [300, 300];
+var initAlive = true;
+
+function registerPlayer()
+{
+    //$("#message").val().trim().length < 1
+    var comment = document.getElementById("playerID").value;
+    if(comment.length == 0 || comment.length>10)
+    {
+        alert("Please enter a valid name!")
+    }
+    else{
+        var messageElement = document.getElementById("playerID");
+        messageElement.value = "";
+        var toSend = JSON.stringify(
+            {
+                "playerID": playerID,
+                "playerHP": initHP,
+                "currentLoc": initLoc,
+                "isAlive": initAlive
+            });
+        ajaxPostRequest("/register", toSend);
+    }
+}
+
+function ajaxPostRequest(path, data){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200){
+            //callback(this.response);
+        }
+    };
+    request.open("POST", path);
+    request.send(data);
+}
