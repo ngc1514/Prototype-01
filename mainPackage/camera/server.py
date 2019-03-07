fac27fc22e79feb5377c9d0afbddee331f54ffc0
Shesky17 <30205154+Shesky17@users.noreply.github.com>
2019-03-07 11:59:36 -0500

    Added showPlayers() function

--- mainPackage/camera/server.py
+++ mainPackage/camera/server.py
@@ -44,6 +44,13 @@ def removePlayer():
     backEnd.removePlayer(content, "playerInfo.json")
 
 
+@app.route('/players', methods=['GET'])
+def showPlayers():
+    with open("playerInfo.json") as f:
+        obj = json.load(f)
+    return json.dumps(obj)
+
+
 if __name__ == '__main__':
     app.run(debug=True)  # host='127.0.0.1')
 
