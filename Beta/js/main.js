0c5d2731a77d684c19b7a2f445a8626a686155f6
Shesky17 <aidenx58@gmail.com>
2019-05-05 18:29:58 -0400

    Multiplayer web app

--- Beta/js/main.js
+++ Beta/js/main.js
@@ -1,6 +1,3 @@
-/**
- * Created by Jerome on 03-03-16.
- */
 //noinspection JSCheckFunctionSignatures,JSCheckFunctionSignatures,JSCheckFunctionSignatures
 var game = new Phaser.Game(1280, 720, Phaser.AUTO, document.getElementById('game'));
 game.state.add('Game',Game);
