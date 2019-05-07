7b35bb160310b4e72112efd5677a02a874ee8e05
Shesky17 <aidenx58@gmail.com>
2019-05-06 20:26:13 -0400

    Multiplayer web app bugs fixed

--- Beta/server.js
+++ Beta/server.js
@@ -11,28 +11,38 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 
+var locList = [];
+
 server.lastPlayderID = 0;
 
 server.listen(process.env.PORT || 8081,function(){
     console.log('Listening on '+server.address().port);
+    makeStone();
+    console.log("listen and make stones:\n" + locList);
 });
 
-io.on('connection',function(socket){
+io.on('connection', function(socket)
+{
+    socket.on('getStones', function(){
+        // console.log("sending to client socket: \n" + locList);
+        socket.emit('giveStones', locList);
+    });
 
     socket.on('newplayer', function()
     {
         socket.player = {
             id: server.lastPlayderID++,
-            x: Math.floor(Math.random()*1280),
-            y: Math.floor(Math.random()*720)
+            x: Math.floor(Math.random()*1500),
+            y: Math.floor(Math.random()*1500)
         };
         console.log("Player ID: " + socket.player.id);
         console.log("Player x: " + socket.player.x + ", Player y: " + socket.player.y);
+
         socket.emit('allplayers', getAllPlayers());
         socket.broadcast.emit('newplayer',socket.player);
 
         socket.on('newLocation', function(data){
-            console.log('player new location: ' + data.x + ', ' + data.y);
+            // console.log('player new location: ' + data.x + ', ' + data.y);
             socket.player.x = data.x;
             socket.player.y = data.y;
             io.emit('move',socket.player);
@@ -48,6 +58,14 @@ io.on('connection',function(socket){
     });
 });
 
+function makeStone(){
+    for(i=0; i<15; i++){
+        var x = rndInRange(200, 1300);
+        var y = rndInRange(200, 1300);
+        locList.push([x,y]);
+    }
+}
+
 function getAllPlayers(){
     var players = [];
     Object.keys(io.sockets.connected).forEach(function(socketID){
@@ -56,4 +74,10 @@ function getAllPlayers(){
             players.push(player);
     });
     return players;
+}
+
+function rndInRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
