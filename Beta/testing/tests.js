994416f2b1acb264089da8f4e5b7f74e2851d07a
jakeheidman <jake.heidman2@gmail.com>
2019-05-10 08:50:57 -0400

    added first js unit test

--- Beta/testing/tests.js
+++ Beta/testing/tests.js
@@ -1,3 +1,21 @@
+var locList = []
+
+function testMath(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function makeStone(){
+    for(i=0; i<10; i++){
+        var x = testMath(100, 1300);
+        var y = testMath(100, 1300);
+        locList.push([x,y]);
+    }
+}
+
+makeStone()
+
 QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
+    assert.ok( locList.length == "1", "Passed!" );
 });
