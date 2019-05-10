4be0d3fbb8c4a71e48ee7f4a75ba1c349ccbe652
jakeheidman <jake.heidman2@gmail.com>
2019-05-10 09:01:43 -0400

    changed enter button on welcome to the IP

--- Beta/server.js
+++ Beta/server.js
@@ -8,7 +8,7 @@ app.use('/js', express.static(__dirname + '/js'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname+'/index.html'); //line to change?
 });
 
 var locList = [];
